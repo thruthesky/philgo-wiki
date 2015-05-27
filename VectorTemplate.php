2820795f18d847f8eb5c043ff16a53dba3d33d46
JaeHo Song <jaeho@gmail.com>
2015-05-28 02:26:53 +0900

    work for frame resize

--- VectorTemplate.php
+++ VectorTemplate.php
@@ -255,6 +255,30 @@ class VectorTemplate extends BaseTemplate {
 		</div>
 		<?php $this->printTrail(); ?>
 
+	<script>
+		var $ = jQuery;
+		var resize_count = 0;
+		$(window).load(function () {
+			resize_iframe();
+		});
+		$(function(){
+			//resize_iframe();
+			setTimeout(function () { resize_iframe(); }, 500);	// resize after 1 second for sure.
+			setTimeout(function () { resize_iframe(); }, 3000);	// resize after 3 seconds for sure.
+			setTimeout(function () { resize_iframe(); }, 5000);	// resize after 5 seconds for sure.
+			setTimeout(function () { resize_iframe(); }, 15000);	// resize after 15 seconds for sure.
+			//setTimeout(function () { resize_iframe(); }, 30000);	// resize after 30 seconds for sure.
+			setTimeout(function () { resize_iframe(); }, 60000);	// resize after 1 MINUTE for sure.
+			// if it takes more than 1 minute, then let's assume that the internet is too slow.
+		});
+		function resize_iframe()
+		{
+			resize_count ++;
+			var height = $(document).height();
+			var data = { 'code' : 'resize', 'height': height, 'count': resize_count };
+			parent.postMessage( data, '*' );
+		}
+	</script>
 	</body>
 </html>
 <?php
